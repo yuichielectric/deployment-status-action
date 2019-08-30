f6c441cf26a776c92a9a4eb0a1adc16c36f9c7ca
Yuichi Tanaka <yuichielectric@github.com>
2019-08-30 09:55:09 +0900

    Implement creating deployment status action

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { ReposCreateDeploymentStatusParams } from '@octokit/rest';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const token = process.env['GITHUB_TOKEN'];
+    if (token == undefined) {
+      return;
+    }
+    const octokit = new github.GitHub(token);
+    const context = github.context;
+    const deploymentId = parseInt(core.getInput('deployment-id'));
+    if (deploymentId == undefined) {
+      return;
+    }
+    const state = core.getInput('state') as ReposCreateDeploymentStatusParams["state"];
+    if (state == undefined) {
+      return;
+    }
+    octokit.repos.createDeploymentStatus({
+      ...context.repo,
+      deployment_id: deploymentId,
+      state: state
+    });
   } catch (error) {
     core.setFailed(error.message);
   }

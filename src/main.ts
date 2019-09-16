51aad15bc34c1652e501c820af156c81db5ecc95
Yuichi Tanaka <yuichielectric@github.com>
2019-09-16 12:16:23 -0700

    Refactor

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import { ReposCreateDeploymentStatusParams } from '@octokit/rest';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { ReposCreateDeploymentStatusParams } from "@octokit/rest";
 
 async function run() {
-  try {
-    const token = process.env['GITHUB_TOKEN'];
-    if (token == undefined) {
-      core.setFailed('GITHUB_TOKEN needs to be passed through environment variable.');
-      return;
-    }
-    const octokit = new github.GitHub(token);
-    const context = github.context;
-    const deploymentId = parseInt(core.getInput('deployment_id', {required: true}));
-    const state = core.getInput('state', {required: true}) as ReposCreateDeploymentStatusParams["state"];
+  const token = process.env["GITHUB_TOKEN"];
+  if (token === undefined) {
+    core.setFailed("GITHUB_TOKEN environment variable is required.");
+    return;
+  }
+  const octokit = new github.GitHub(token);
+  const context = github.context;
+  const deploymentId = parseInt(
+    core.getInput("deployment_id", { required: true }),
+    10
+  );
+  const state = core.getInput("state", {
+    required: true
+  }) as ReposCreateDeploymentStatusParams["state"];
 
-    const target_url = core.getInput('target_url');
+  const targetUrl = core.getInput("target_url");
+  try {
     octokit.repos.createDeploymentStatus({
       ...context.repo,
       deployment_id: deploymentId,
       state: state,
-      target_url: target_url
+      target_url: targetUrl
     });
   } catch (error) {
     core.setFailed(error.message);
